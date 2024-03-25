99e7559c1f30bc161cc88520654fac44c5dd1ff6
Strke12i <felipebagesteiro@gmail.com>
2024-03-24 23:28:45 -0300

    Task N°:6 - Creating Tag Model and implementing user relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_24_191052) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_25_022036) do
   create_table "contents", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_24_191052) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "user_id"], name: "index_tags_on_name_and_user_id", unique: true
+    t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +42,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_24_191052) do
   end
 
   add_foreign_key "contents", "users"
+  add_foreign_key "tags", "users"
 end
